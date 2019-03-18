d3e54358c63f64a155c0bb2016a957e1812c1deb
devayanip <pawardevayani20@gmail.com>
2019-03-18 15:42:52 +0530

    ExtentRe

--- src/test/java/com/test/GitProject_p/Git_Class.java
+++ src/test/java/com/test/GitProject_p/Git_Class.java
@@ -1,6 +1,13 @@
 package com.test.GitProject_p;
 
 import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReporter;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
 import org.testng.annotations.BeforeTest;
 
 import java.util.concurrent.TimeUnit;
@@ -14,6 +21,9 @@ import org.testng.annotations.AfterTest;
 public class Git_Class 
 {
 	WebDriver driver;
+	ExtentHtmlReporter report;
+	ExtentReports e;
+	ExtentTest t;
 	
   @Test
   public void sign() throws InterruptedException 
@@ -33,6 +43,17 @@ public class Git_Class
   @BeforeTest
   public void beforeTest() 
   {
+	  report=new ExtentHtmlReporter("Resource/makereport.html");
+	  report.config().setTheme(Theme.DARK);
+	  report.config().setChartVisibilityOnOpen(true);
+	  report.setAppendExisting(true);
+	  
+	  e=new ExtentReports();
+	  e.attachReporter(report);
+	  
+	  t=e.createTest("newtest");
+	  t.pass("Started test");
+	  
 	  System.setProperty("webdriver.chrome.driver", "Resource/chromedriver.exe");
 	  
 	  driver=new ChromeDriver();
@@ -47,7 +68,9 @@ public class Git_Class
   @AfterTest
   public void afterTest() 
   {
+	  t.info("Finished test");
 	  driver.quit(); 
+	  e.flush();
   }
 
 }
