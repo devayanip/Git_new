fe0aa5ad89c88910112b6ad555596bdafce99307
devayanip <pawardevayani20@gmail.com>
2019-03-18 13:12:39 +0530

    first commit

---
+++ src/test/java/com/test/GitProject_p/Git_Class.java
@@ -0,0 +1,51 @@
+package com.test.GitProject_p;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+
+public class Git_Class 
+{
+	WebDriver driver;
+	
+  @Test
+  public void sign() 
+  {
+	  WebElement signbutton=driver.findElement(By.xpath("//a[@title='Log in to your customer account']"));  
+	  signbutton.click();
+	  
+	  WebElement email=driver.findElement(By.id("email_create"));
+	  email.sendKeys("sam@gmail.com");
+	  
+	  WebElement submit=driver.findElement(By.id("SubmitCreate"));
+	  submit.click();
+  }
+  
+  @BeforeTest
+  public void beforeTest() 
+  {
+	  System.setProperty("webdriver.chrome.driver", "Resource/chromedriver.exe");
+	  
+	  driver=new ChromeDriver();
+	  
+	  driver.manage().window().maximize();
+	  
+	  driver.get("http://automationpractice.com/index.php");
+	  
+	  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+  }
+
+  @AfterTest
+  public void afterTest() 
+  {
+	  driver.quit(); 
+  }
+
+}
